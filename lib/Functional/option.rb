f1b1a97ba021e8403dad8f93f063133fb97007c0
Артём Большаков <abolshakov@spbtv.com>
2015-02-11 01:19:06 +0300

    Adopt documentation from Scala's Option

--- lib/Functional/option.rb
+++ lib/Functional/option.rb
@@ -1,4 +1,39 @@
 module Functional
+  # Represents optional values. Instances of `Option`
+  #  are either an instance of $some or the object $none.
+  #
+  #  The most idiomatic way to use an $option instance is to treat it
+  #  as a collection or monad and use `map`,`flat_map`, `select`, or
+  #  `each`:
+  #
+  #  {{{
+  #  name = Option(params[:name])
+  #  upper = name.map(&:strip).select { |n| n.length != 0 }.map(&:upcase)
+  #  puts upper.get_or_else('')
+  #  }}}
+  #
+  #  Because of how for comprehension works, if $none is returned
+  #  from `Option(params[:name])`, the entire expression results in
+  #  $none
+  #
+  #  This allows for sophisticated chaining of $option values without
+  #  having to check for the existence of a value.
+  #
+  #  A less-idiomatic way to use $option values is via pattern matching: {{{
+  #  name_maybe = Option(params[:name])
+  #  case name_maybe
+  #  when Some
+  #    puts name.strip.upcase
+  #  when None
+  #    puts 'No name value'
+  #  end
+  #  }}}
+  #
+  #  @note Many of the methods in here are duplicative with those
+  #  in the Traversable hierarchy, but they are duplicated for a reason:
+  #  the implicit conversion tends to leave one with an Iterable in
+  #  situations where one could have retained an Option.
+  #
   module Option
     def self.empty
       None()
