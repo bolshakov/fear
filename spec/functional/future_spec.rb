7e68d2560d99c0317819a5e6d8884ebdde0636a4
Артём Большаков <abolshakov@spbtv.com>
2015-02-27 01:23:14 +0300

    Future#on_success specs

--- spec/functional/future_spec.rb
+++ spec/functional/future_spec.rb
@@ -25,4 +25,22 @@ RSpec.describe Future do
       end.to yield_with_args(Failure(error))
     end
   end
+
+  context '#on_success' do
+    it 'run callback if no error' do
+      expect do |callback|
+        await do
+          Future { value }.on_success(&callback)
+        end
+      end.to yield_with_args(value)
+    end
+
+    it 'do not run callback if error occured' do
+      expect do |callback|
+        await do
+          Future { fail error }.on_success(&callback)
+        end
+      end.not_to yield_with_args
+    end
+  end
 end
