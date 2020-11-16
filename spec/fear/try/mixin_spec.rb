8ff301ee2b1b3670c25cf8c4c7b81e00533acc8b
Tema Bolshakov <either.free@gmail.com>
2020-11-16 11:11:36 +0300

    Add specs for Fear::Try::Mixin

--- spec/fear/try/mixin_spec.rb
+++ spec/fear/try/mixin_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe Fear::Try::Mixin do
       it { is_expected.to be_kind_of(Fear::Failure) }
     end
   end
+
+  describe "Success()" do
+    subject { Success(42) }
+
+    it { is_expected.to be_success_of(42) }
+  end
+
+  describe "Failure()" do
+    subject { Failure(error) }
+
+    let(:error) { StandardError.new }
+
+    it { is_expected.to be_failure_of(error) }
+  end
 end
