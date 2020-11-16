7dfa4a6baba1351626698dc2d9b20cddfca84640
Tema Bolshakov <either.free@gmail.com>
2020-11-16 11:41:52 +0300

    Test chaining guards and Any

--- spec/fear/guard_spec.rb
+++ spec/fear/guard_spec.rb
@@ -77,6 +77,87 @@ RSpec.describe Fear::PartialFunction::Guard do
         expect { guard === 4 }.not_to raise_error
       end
     end
+
+    context "with many arguments" do
+      let(:guard) { Fear::PartialFunction::Guard.and([guard_1, guard_2, guard_3, guard_4]) }
+
+      let(:guard_1) { ->(x) { x % 5 == 0 } }
+      let(:guard_2) { ->(x) { x % 2 == 0 } }
+      let(:guard_3) { ->(x) { x % 6 == 0 } }
+      let(:guard_4) { ->(x) { x % 7 == 0 } }
+      let(:guard_4) { ->(x) { x % 8 == 0 } }
+
+      it { expect(guard === 3360).to eq(true) }
+      it { expect(guard === 10).to eq(false) }
+      it { expect(guard === 8).to eq(false) }
+      it { expect(guard === 5).to eq(false) }
+      it { expect(guard === 7).to eq(false) }
+      it { expect(guard === 6).to eq(false) }
+      it { expect(guard === 2).to eq(false) }
+    end
+
+    describe "#and" do
+      let(:guard) { guard_1_and_guard_2.and(guard_3) }
+      let(:guard_1_and_guard_2) { Fear::PartialFunction::Guard.and([guard_1, guard_2]) }
+
+      let(:guard_1) { ->(x) { x % 5 == 0 } }
+      let(:guard_2) { ->(x) { x % 2 == 0 } }
+      let(:guard_3) { ->(x) { x % 6 == 0 } }
+
+      it { expect(guard === 30).to eq(true) }
+      it { expect(guard === 10).to eq(false) }
+      it { expect(guard === 5).to eq(false) }
+      it { expect(guard === 2).to eq(false) }
+      it { expect(guard === 3).to eq(false) }
+    end
+
+    describe "#and", "with three arguments" do
+      let(:guard) { guard_123.and(guard_4) }
+      let(:guard_123) { Fear::PartialFunction::Guard.and([guard_1, guard_2, guard_3]) }
+
+      let(:guard_1) { ->(x) { x % 5 == 0 } }
+      let(:guard_2) { ->(x) { x % 2 == 0 } }
+      let(:guard_3) { ->(x) { x % 6 == 0 } }
+      let(:guard_4) { ->(x) { x % 7 == 0 } }
+
+      it { expect(guard === 420).to eq(true) }
+      it { expect(guard === 10).to eq(false) }
+      it { expect(guard === 5).to eq(false) }
+      it { expect(guard === 7).to eq(false) }
+      it { expect(guard === 6).to eq(false) }
+      it { expect(guard === 2).to eq(false) }
+    end
+
+    describe "#or" do
+      let(:guard) { guard_1_and_guard_2.or(guard_3) }
+      let(:guard_1_and_guard_2) { Fear::PartialFunction::Guard.and([guard_1, guard_2]) }
+
+      let(:guard_1) { ->(x) { x % 5 == 0 } }
+      let(:guard_2) { ->(x) { x % 2 == 0 } }
+      let(:guard_3) { ->(x) { x % 3 == 0 } }
+
+      it { expect(guard === 10).to eq(true) }
+      it { expect(guard === 3).to eq(true) }
+      it { expect(guard === 5).to eq(false) }
+      it { expect(guard === 2).to eq(false) }
+      it { expect(guard === 7).to eq(false) }
+    end
+
+    describe "#or", "with three arguments" do
+      let(:guard) { guard_123.or(guard_4) }
+      let(:guard_123) { Fear::PartialFunction::Guard.and([guard_1, guard_2, guard_3]) }
+
+      let(:guard_1) { ->(x) { x % 5 == 0 } }
+      let(:guard_2) { ->(x) { x % 2 == 0 } }
+      let(:guard_3) { ->(x) { x % 6 == 0 } }
+      let(:guard_4) { ->(x) { x % 7 == 0 } }
+
+      it { expect(guard === 30).to eq(true) }
+      it { expect(guard === 7).to eq(true) }
+      it { expect(guard === 5).to eq(false) }
+      it { expect(guard === 2).to eq(false) }
+      it { expect(guard === 6).to eq(false) }
+    end
   end
 
   describe ".or" do
@@ -98,6 +179,35 @@ RSpec.describe Fear::PartialFunction::Guard do
       subject { guard === "A&" }
 
       it { is_expected.to eq(false) }
+    end
+
+    describe "#and" do
+      let(:guard) { guard_1_or_guard_2.and(guard_3) }
+      let(:guard_1_or_guard_2) { Fear::PartialFunction::Guard.or([guard_1, guard_2]) }
+
+      let(:guard_1) { ->(x) { x % 5 == 0 } }
+      let(:guard_2) { ->(x) { x % 2 == 0 } }
+      let(:guard_3) { ->(x) { x % 3 == 0 } }
+
+      it { expect(guard === 6).to eq(true) }
+      it { expect(guard === 15).to eq(true) }
+      it { expect(guard === 5).to eq(false) }
+      it { expect(guard === 2).to eq(false) }
+      it { expect(guard === 3).to eq(false) }
+    end
+
+    describe "#or" do
+      let(:guard) { guard_1_or_guard_2.or(guard_3) }
+      let(:guard_1_or_guard_2) { Fear::PartialFunction::Guard.or([guard_1, guard_2]) }
+
+      let(:guard_1) { ->(x) { x % 5 == 0 } }
+      let(:guard_2) { ->(x) { x % 2 == 0 } }
+      let(:guard_3) { ->(x) { x % 3 == 0 } }
+
+      it { expect(guard === 5).to eq(true) }
+      it { expect(guard === 2).to eq(true) }
+      it { expect(guard === 3).to eq(true) }
+      it { expect(guard === 7).to eq(false) }
     end
   end
 end
